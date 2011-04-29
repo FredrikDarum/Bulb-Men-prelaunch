010c97549bf4d383b017f39acd62b0bad777ff94
signalkrem <signalkrem@gmail.com>
2011-04-29 18:47:30 +0200

    -a -d
    Signed-off-by: signalkrem <signalkrem@gmail.com>

---
+++ Bulb-Men___We_Got_Nerdpowers.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.3 local-build-20110429
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:9,offset:new IWPoint(-0.0000,3.0000),color:'#000000',opacity:0.280000}),shadow_1:new IWShadow({blurRadius:5,offset:new IWPoint(-0.0000,5.0000),color:'#000000',opacity:0.500000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Bulb-Men___We_Got_Nerdpowers_files/Bulb-Men___We_Got_NerdpowersMoz.css')
+fixAllIEPNGs('Media/transparent.gif');applyEffects()}
